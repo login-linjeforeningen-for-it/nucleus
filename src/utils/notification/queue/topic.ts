21b66c9ac627f1802ad5b896ea09134f101091db
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-28 07:25:04 +0200

    Trim notification queue docs

--- src/utils/notification/queue/topic.ts
+++ src/utils/notification/queue/topic.ts
@@ -1,109 +1,3 @@
-/**
- * This is a complex file, therefore a queue system has been implemented.
- * We allow the first request, then store the most recent request at all times
- * while the first one is working. For example if we have 10 requests, number 1
- * will execute, and 2 will be stored, then when 3 comes in, 3 will override 2,
- * which continues until we are left with request 1 and 10. When number 1 is
- * done, number 10 will run. If this keeps going, the loop will be infinite
- * until the requests stop.
- *
- * When a request comes in, it first sets up throttling so all other requests
- * are stored or dropped if they are not the last one nor unique. Then we check
- * if the request is for a language change or a notification state change. These
- * are handled differently, and therefore have different queues.
- *
- * Language changes require all currently subscribed topics to be converted to
- * the new language, except events (might be implemented in the future). The
- * topics for the previous language will be unsubscribed, as the new ones are
- * added. When everything has completed, the function will execute again if
- * there is another language request in the queue.
- *
- * Notification state requests will check all notification states, not just
- * itself, due to the nature of the state being an array, and Redux update
- * limits. This is the type where throttling is the most important, as you can
- * see how hundreds of request can quickly accumulate as one button click equals
- * about 60 requests. However with throttling, you will only get a few requests
- * per second, even when the user is subscribing in complex patterns in a fast
- * manner. After executing successfully, the function will check if there are
- * more notification state requests that have not been processed yet, and if so
- * execute the last one.
- *
- *                      +----------------------------------+
- *                      |              topic()             |
- *                      +----------------------------------+
- *                                       |
- *                      +----------------------------------+
- *                      |         Check Throttling         |
- *                      |         and Queuing Logic        |
- *                      +----------------------------------+
- *                                        |
- *                    +-------------------+-------------------+
- *                    |                                       |
- *       +------------+-------------+           +-------------+------------+
- *       |                          |           |                          |
- *       |   Process Notification   |           |      Process Language    |
- *       |      State Requests      |           |      Change Requests     |
- *       |                          |           |                          |
- *       +------------+-------------+           +-------------+------------+
- *                    |                                       |
- *                    +-------------------+-------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |     Find and proceed     |
- *                           |    with changed states   |
- *                           |                          |
- *                           +--------------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |       Topic Manager      |
- *                           |                          |
- *                           +--------------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |   Update Subscriptions   |
- *                           |   and Unsubscriptions    |
- *                           |                          |
- *                           +--------------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |    Redux State Updates   |
- *                           |                          |
- *                           +--------------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |     Execute Pending      |
- *                           |     Request if Queued    |
- *                           |                          |
- *                           +--------------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |    Check Throttling      |
- *                           |    and Queuing Logic     |
- *                           |                          |
- *                           +--------------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |    Process Notification  |
- *                           |     Change Requests in   |
- *                           |       Queue (repeat)     |
- *                           |                          |
- *                           +--------------------------+
- *                                        |
- *                           +------------+-------------+
- *                           |                          |
- *                           |     Lift Throttling      |
- *                           |     When Empty Queue     |
- *                           |                          |
- *                           +--------------------------+
- */
-
 import { Dispatch, UnknownAction } from 'redux'
 import TopicManager from '../topicManager'
 import { setNotificationDidUpdateRemotely } from '@redux/notifications'
@@ -120,6 +14,11 @@ const throttled = [false, false]
 let langChangeQueue: topicParams | null
 let notificationStateQueue: topicParams | null
 
+/**
+ * Topic updates are throttled by request type. The first request runs
+ * immediately; while it is running, only the latest pending request is kept so
+ * rapid switch toggles collapse into one final sync.
+ */
 /**
  * **Function for subscribing and unsubscribing from notification topics**
  *
